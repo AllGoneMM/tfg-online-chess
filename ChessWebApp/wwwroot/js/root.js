965efa297f98b43decb20b9c123e4c8f00fcb77a
Mykyta Metelytsya <shadyrnb@gmail.com>
2024-06-06 14:46:15 +0200

    Add function to adjust height dynamically

---
+++ ChessWebApp/wwwroot/js/root.js
@@ -0,0 +1,14 @@
+﻿window.onload = function () {
+    adjustContentHeight();
+};
+
+window.onresize = function () {
+    adjustContentHeight();
+};
+
+function adjustContentHeight() {
+    var navbarHeight = document.getElementById('navbar').offsetHeight;
+    let rowContent = document.getElementById('rowContent');
+    rowContent.style.cssText = "min-height: calc(100vh - " + navbarHeight + "px) !important;";
+}
+
