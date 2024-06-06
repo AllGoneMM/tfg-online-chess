fd3843431b956bf3413c1fb866f30ed2a081cf5d
Mykyta Metelytsya <shadyrnb@gmail.com>
2024-06-06 14:43:45 +0200

    Add Play controller and views

---
+++ ChessWebApp/Controllers/PlayController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChessWebApp.Controllers
+{
+    public class PlayController : Controller
+    {
+        [HttpGet("/online")]
+        public IActionResult Online()
+        {
+            return View();
+        }
+        [HttpGet("/offline")]
+        public IActionResult Offline()
+        {
+            return View();
+        }
+    }
+}
