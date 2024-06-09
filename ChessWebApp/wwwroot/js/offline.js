30b1cc22eb9d46b3d3519cd708df5d201a77f69c
Mykyta <shadyrnb@gmail.com>
2024-06-09 20:04:31 +0200

    Chessboard progress

--- ChessWebApp/wwwroot/js/offline.js
+++ ChessWebApp/wwwroot/js/offline.js
@@ -1,22 +1,31 @@
 ﻿var board2;
-window.onload = function () {
-    adjustContentHeight();
-    board2 = Chessboard('board2', {
-        pieceTheme: 'assets/img/chesspieces/{piece}.png',
-        draggable: true,
-        dropOffBoard: 'trash',
-    })
+document.addEventListener('DOMContentLoaded',
+    function() {
+        board2 = Chessboard('board2', {
+            pieceTheme: 'assets/img/chesspieces/{piece}.png',
+            draggable: true,
+            dropOffBoard: 'trash'
+        });
 
-    $('#startBtn').on('click', board2.start)
-    $('#clearBtn').on('click', board2.clear)
-}
+        document.getElementById('startBtn').addEventListener('click', board2.start);
+        document.getElementById('clearBtn').addEventListener('click', board2.clear);
+        adjustContentHeight();
+        board2.resize();
+    });
 
 window.onresize = function () {
     adjustContentHeight();
     board2.resize();
-}
+};
+
 function adjustContentHeight() {
-    var navbarHeight = document.getElementById('navbar').offsetHeight;
-    let rowContent = document.getElementById('boardContent');
-    rowContent.style.cssText = "max-height: calc(100dvh - " + navbarHeight + "px) !important;";
+    let boardContainer = document.getElementById('boardContainer');
+    let navbarHeight = document.getElementById('navbar').offsetHeight;
+    let opponentDivHeight = document.getElementById('opponentDiv').offsetHeight;
+    let userDivHeight = document.getElementById('userDiv').offsetHeight;
+    let availableHeight = window.innerHeight - navbarHeight - opponentDivHeight - userDivHeight;
+    let availableWidth = boardContainer.offsetWidth;
+
+    let boardSize = Math.min(availableHeight, availableWidth);
+    document.getElementById('board2').style.width = `${boardSize}px`;
 }
