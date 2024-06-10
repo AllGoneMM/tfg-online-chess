36bd18497148b8c0251f44f97dfbb89605b2cbae
Mykyta <shadyrnb@gmail.com>
2024-06-10 20:53:05 +0200

    Modify chessboardjs and offline.js

--- ChessWebApp/wwwroot/js/offline.js
+++ ChessWebApp/wwwroot/js/offline.js
@@ -1,48 +1,74 @@
 ﻿var chessBoard;
+var chessGame;
 var signalRConnection;
-
 document.addEventListener('DOMContentLoaded', async function () {
-
-    document.getElementById("startGame").addEventListener("click", async function () {
-        try {
-            let serverJson = await signalRConnection.invoke("StartGame");
-            chessBoard = Chessboard('chessBoard', {
-                pieceTheme: 'assets/img/chesspieces/{piece}.png',
-                draggable: true,
-                position: 'start',
-                onDrop: await onDrop
-            });
-        } catch (err) {
-            alert("Error al iniciar una nueva partida: " + err.toString());
-        }
-    });
-
-
+    // Start the connection with OfflineGameHub
     signalRConnection = new signalR.HubConnectionBuilder()
         .withUrl("/hubs/offlinegame", signalR.HttpTransportType.WebSockets)
         .withAutomaticReconnect()
         .build();
-
     signalRConnection.start()
-        .catch(error => alert("Error al abrir la conexión: " + error.toString()));
+        .catch(error => alert("Error al conectar con el servidor"));
+
+    // Event listener for the start game button
+    document.getElementById("startGame").addEventListener("click", async function () {
+
+        let serverJson = await signalRConnection.invoke("StartGame")
+            .then(() => {
+                chessBoard = Chessboard('chessBoard', {
+                    pieceTheme: 'assets/img/chesspieces/{piece}.png',
+                    draggable: true,
+                    moveSpeed: 'slow',
+                    snapbackSpeed: 500,
+                    snapSpeed: 100,
+                    position: 'start',
+                    onDrop: onDrop,
+                    onDragStart: onDragStart
+                });
+            })
+            .catch((error) => {
+                window.alert("Error al iniciar la partida "+error.toString());
+            });
+    });
 });
 
 async function onDrop(source, target, piece, newPos, oldPos, orientation) {
     let move = source + target;
-    return 'snapback'
-    try {
-        let chessGame = await signalRConnection.invoke("ProcessMove", move);
+    await signalRConnection.invoke("ProcessMove", move)
+        .then((response) => {
 
-        chessGame = JSON.parse(chessGame);
-        let fen = chessGame.Fen;
-        let state = chessGame.State;
-        let promotion = chessGame.Promotion;
-        chessBoard.position(fen);
-        if(state != 1) {
-            const exampleModal = new bootstrap.Modal(document.getElementById('exampleModal'))
-            exampleModal.show();
-        }
-    } catch (err) {
-        chessBoard.position(oldPos);
+        })
+        .catch(() => {
+            window.alert("Error al procesar el movimiento");
+            return 'snapback';
+        });
+
+}
+//function onSnapEnd() {
+//    chessBoard.position(updatedBoar);
+//}
+
+async function onDragStart(source, piece, position, orientation) {
+    await signalRConnection.invoke("GetLegalMoves", source)
+        .then((response) => {
+            response = JSON.parse(response);
+            for (var i = 0; i < response.length; i++) {
+                greySquare(response[i]);
+            }
+        })
+        .catch(() => {
+            window.alert("Error al obtener movimientos");
+            return 'snapback';
+        });
+}
+
+function greySquare(square) {
+    var $square = $('#chessBoard .square-' + square);
+
+    var background = '#a9a9a9';
+    if ($square.hasClass('black-3c85d')) {
+        background = '#696969';
     }
+
+    $square.css('background', background);
 }
